b6977c20d695b2b050510e6772931a343b7f1ca3
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-19 19:40:07 +0200

    Test Site internet

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// script.js - Version Niveau 2 de secours
-console.log("Tentative de lancement du Niveau 2...");
-
-WA.onInit().then(() => {
-    console.log("API WorkAdventure prête !");
-
-    // On ajoute un petit délai de 1 seconde pour être sûr que Lulu est bien là
-    setTimeout(() => {
-        WA.ui.displayBubble({
-            label: "Welcome! Follow the arrows to discover our Community Framework.",
-            position: "top",
-            duration: 7000
-        });
-        console.log("Bulle envoyée !");
-    }, 1000);
-});
+WA.chat.sendChatMessage('Hello world', 'Mr Robot');
