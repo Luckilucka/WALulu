393731d25baf742096d64b18d0195a026e00d7cd
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-19 19:23:50 +0200

    Niveau 2 js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// script.js (Version pur JavaScript pour GitHub Pages)
+// script.js - Niveau 2
 WA.onInit().then(() => {
-    console.log("Niveau 1 : Connexion GitHub Pages réussie !");
+    console.log("Niveau 2 : Système d'accueil activé.");
+
+    // Affiche une bulle de texte au-dessus du joueur
+    WA.ui.displayBubble({
+        label: "Welcome to the EEG101 virtual lab! Follow the arrows to start your journey.",
+        position: "top",
+        duration: 7000 // La bulle reste visible 7 secondes pour être bien lue
+    });
 });
