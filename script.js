e131bef30f190e31c377a0262da7aa21b6ba44c3
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-20 10:08:58 +0200

    Forcage via script js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-// script.js - Niveau 3 : Interaction Zone
-WA.onInit().then(() => {
-    console.log("Système d'interaction prêt. Marche sur la zone de Bob !");
+// script.js - Diagnostic Niveau 3
+console.log("FORCE LOAD : Le fichier script.js est bien lu par le navigateur !");
 
-    // On écoute l'entrée dans la zone 'npc_bob'
+WA.onInit().then(() => {
+    console.log("WA INITIALIZED : Lulu est prête. Marche vers Bob (x:244, y:362)");
+
+    // On déclenche le popup quand on entre dans la zone npc_bob
     WA.room.area.onEnter("npc_bob").subscribe(() => {
-        console.log("Lulu est entrée dans la zone de Bob !");
-        
+        console.log("ZONE ENTER : Contact avec Bob réussi !");
         WA.ui.openPopup("bob_popup", "Welcome to the Validity section! Robust data is the foundation of our science.", [
             {
-                label: "I understand",
+                label: "Understood",
                 className: "primary",
                 callback: (popup) => {
                     popup.close();
                 }
             }
         ]);
-        console.log("Lulu a vu le pop up !");
+        console.log("Lulu est entrée dans la zone de Bob !");
+        
     });
 });
