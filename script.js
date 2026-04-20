5c576a7ba1e45d6f9b67da73e469ca4e064f0ca8
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-20 09:48:35 +0200

    Tentative bulle

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-WA.chat.sendChatMessage('Hello world', { scope: 'bubble' });
+// script.js - Niveau 2 (Version validée)
+WA.onInit().then(() => {
+    // Ce message confirme que la poignée de main a réussi
+    console.log("WA initialisé. Lancement de la bulle d'accueil...");
+
+    // On affiche la bulle au-dessus de Lulu
+    WA.ui.displayBubble({
+        label: "Salut bg.",
+        position: "top",
+        duration: 15000 // Affichage pendant 5 secondes
+    });
+
+   console.log("La bulle a été lancée !");
+});
