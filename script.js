31563cd173290d27c5c2cbb9e9200cdfbd811657
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-20 10:00:40 +0200

    Tentative avec npc_bob

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// script.js - Test de Popup (Niveau 3)
+// script.js - Niveau 3 : Interaction Zone
 WA.onInit().then(() => {
-    console.log("WA initialisé. Tentative d'ouverture de Popup...");
+    console.log("Système d'interaction prêt. Marche sur la zone de Bob !");
 
-    // Au lieu d'une bulle, on ouvre une fenêtre au milieu de l'écran
-    WA.ui.openPopup("intro_popup", "Welcome to the EEG101 Virtual Lab! Here you will learn about scientific validity and responsibility.", [
-        {
-            label: "Let's go!",
-            className: "primary",
-            callback: (popup) => {
-                popup.close(); // Ferme le popup quand on clique
-                console.log("L'utilisateur a fermé le popup.");
+    // On écoute l'entrée dans la zone 'npc_bob'
+    WA.room.area.onEnter("npc_bob").subscribe(() => {
+        console.log("Lulu est entrée dans la zone de Bob !");
+        
+        WA.ui.openPopup("bob_popup", "Welcome to the Validity section! Robust data is the foundation of our science.", [
+            {
+                label: "I understand",
+                className: "primary",
+                callback: (popup) => {
+                    popup.close();
+                }
             }
-        }
-    ]);
-
-    console.log("Ca a marché ...???");
-
-    
+        ]);
+        console.log("Lulu a vu le pop up !");
+    });
 });
